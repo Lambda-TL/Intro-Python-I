6ae27923f40f567438e31eb41dbb317e08b4405f
Jessica <duellje@oregonstate.edu>
2020-10-04 18:34:22 -0400

    notes

--- src/00_hello.py
+++ src/00_hello.py
@@ -1,1 +1,21 @@
 # Print "Hello, world!" to your terminal
+# print("Hello, world!")
+
+greeting = "Hello, world!"
+number1 = 20
+floatNum = 19.5
+
+# print(greeting, number1, floatNum)
+
+x = 200
+y = 2000
+print("")
+
+if x < y:
+    print('x is less than y')
+elif x == y:
+    print('x is equal to y')
+else:
+    print('x is greater than y')
+    
+print("")
